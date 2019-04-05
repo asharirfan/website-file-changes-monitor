6843e6cbb9852f53c78c2b215c2faf5a1f12e5d7
Ashar Irfan <mrasharirfan@gmail.com>
2019-04-05 15:59:49 +0500

    📦 NEW: Grouped Event Class

---
+++ includes/event/class-wpfcm-event-grouped.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * WPFCM Grouped Event.
+ *
+ * @package wpfcm
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * WPFCM Grouped Event Class.
+ *
+ * Handle the events of grouped files like when a plugin
+ * is installed, updated, or removed, etc.
+ */
+class WPFCM_Event_Grouped extends WPFCM_Event {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param integer $event_id - (Optional) Event id.
+	 */
+	public function __construct( $event_id = 0 ) {
+		$this->data['content_type'] = 'directory'; // Content type.
+		parent::__construct( $event_id );
+	}
+
+	/**
+	 * Set content type.
+	 *
+	 * @param string $content_type - Content type.
+	 * @return string
+	 */
+	public function set_content_type( $content_type ) {
+		return $this->set_meta( 'content_type', $content_type );
+	}
+
+	/**
+	 * Returns content type.
+	 *
+	 * @return string
+	 */
+	public function get_content_type() {
+		return $this->get_meta( 'content_type' );
+	}
+}
