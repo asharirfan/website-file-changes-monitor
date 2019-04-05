2de7ebbe37033965a24323d023c4f7cbcff0f20a
Ashar Irfan <mrasharirfan@gmail.com>
2019-04-05 18:12:39 +0500

    📦 NEW: File changes script

---
+++ assets/js/custom/file-changes.js
@@ -0,0 +1,12 @@
+/**
+ * File Changes
+ */
+
+// import { ReactDOM } from "react-dom";
+
+// window.addEventListener( 'load', function() {
+
+const pEl = wp.element.createElement( 'h1', {}, 'Hello WP from React.' );
+wp.element.render( pEl, document.querySelector( '#wpfcm-file-changes-views' ) );
+
+// });
---
+++ assets/js/file-changes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * File Changes
+ */
+// import { ReactDOM } from "react-dom";
+// window.addEventListener( 'load', function() {
+var pEl = wp.element.createElement('h1', {}, 'Hello WP from React.');
+wp.element.render(pEl, document.querySelector('#wpfcm-file-changes-views')); // });
