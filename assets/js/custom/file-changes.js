c820d9ffdb4f465bf49027f224f4c81d86932432
Ashar Irfan <mrasharirfan@gmail.com>
2019-04-09 13:05:26 +0500

    👌 IMPROVE: File changes script src and dist files

--- assets/js/custom/file-changes.js
+++ assets/js/custom/file-changes.js
@@ -1,12 +1,7 @@
 /**
  * File Changes
  */
+import ReactDOM from 'react-dom';
+import FileChanges from './file-changes/FileChanges';
 
-// import { ReactDOM } from "react-dom";
-
-// window.addEventListener( 'load', function() {
-
-const pEl = wp.element.createElement( 'h1', {}, 'Hello WP from React.' );
-wp.element.render( pEl, document.querySelector( '#wpfcm-file-changes-views' ) );
-
-// });
+ReactDOM.render( <FileChanges />, document.getElementById( 'wpfcm-file-changes-views' ) );
