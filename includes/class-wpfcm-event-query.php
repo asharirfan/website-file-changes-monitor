644e5c8b2a4e134701da613ea0b9d706f3107646
Ashar Irfan <mrasharirfan@gmail.com>
2019-04-11 14:03:42 +0500

    📦 NEW: Event query class

---
+++ includes/class-wpfcm-event-query.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * WPFCM Events Query.
+ *
+ * @package wpfcm
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Events Query Class.
+ */
+class WPFCM_Event_Query {
+
+	/**
+	 * Query variables.
+	 *
+	 * @var array
+	 */
+	protected $query_vars = array();
+
+	/**
+	 * Constructor.
+	 *
+	 * @param array $args - Array of query arguments.
+	 */
+	public function __construct( $args = array() ) {
+		$this->query_vars = wp_parse_args( $args, $this->get_default_query_vars() );
+	}
+
+	/**
+	 * Returns query arguments.
+	 *
+	 * @return array
+	 */
+	private function get_args() {
+		return $this->query_vars;
+	}
+
+	/**
+	 * Returns default arguments for quering events from WordPress.
+	 *
+	 * @return array
+	 */
+	private function get_default_query_vars() {
+		return array(
+			'post_status'    => array( 'draft', 'pending', 'private', 'publish' ),
+			'post_type'      => 'wpfcm_event',
+
+			'posts_per_page' => -1,
+			'paginate'       => false,
+
+			'order'          => 'DESC',
+			'orderby'        => 'date',
+
+			'return'         => 'objects',
+
+			'event_type'     => '', // Event type: added, modified, or deleted.
+			'status'         => '', // Event status.
+		);
+	}
+
+	/**
+	 * Get events from WordPress.
+	 *
+	 * @return array|object
+	 */
+	public function get_events() {
+		$args   = $this->get_args();
+		$events = WPFCM_Data_Store::load( 'event' )->query( $args );
+		return $events;
+	}
+}
