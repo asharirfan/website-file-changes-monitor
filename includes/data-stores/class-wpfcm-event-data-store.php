51fe577467be36e4e73604bf5bf88dcfea917e6b
Ashar Irfan <mrasharirfan@gmail.com>
2019-04-11 14:03:31 +0500

    📦 NEW: Event data store classes

---
+++ includes/data-stores/class-wpfcm-event-data-store.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * WPFCM Event Post Type Data Store.
+ *
+ * @package wpfcm
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Event post type data store.
+ */
+class WPFCM_Event_Data_Store {
+
+	/**
+	 * Event meta keys.
+	 *
+	 * @var array
+	 */
+	private $meta_keys = array(
+		'event_type',
+		'status',
+	);
+
+	/**
+	 * Returns WP_Query arguements.
+	 *
+	 * @param array $query_args - Query arguments.
+	 * @return array
+	 */
+	private function get_wp_query_vars( $query_args ) {
+		$wp_query_args = array(
+			'meta_query' => array(), // phpcs:ignore
+		);
+
+		foreach ( $query_args as $key => $value ) {
+			if ( 'meta_query' === $key ) {
+				continue;
+			}
+
+			if ( in_array( $key, $this->meta_keys, true ) ) {
+				if ( ! $value ) {
+					continue;
+				}
+
+				$wp_query_args['meta_query'][] = array(
+					'key'     => $key,
+					'value'   => $value,
+					'compare' => is_array( $value ) ? 'IN' : '=',
+				);
+			} else {
+				$wp_query_args[ $key ] = $value;
+			}
+		}
+
+		return $wp_query_args;
+	}
+
+	/**
+	 * Query events.
+	 *
+	 * @param array $query_args - Query arguments.
+	 * @return array|object
+	 */
+	public function query( $query_args ) {
+		$wp_query_args = $this->get_wp_query_vars( $query_args );
+
+		$query = new WP_Query( $wp_query_args );
+
+		$events = isset( $query->posts ) ? array_map( 'wpfcm_get_event', $query->posts ) : array();
+
+		return $events;
+	}
+}
