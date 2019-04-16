7e7e9dbea5193a32e754d5766b5f6fabf7c77924
Ashar Irfan <mrasharirfan@gmail.com>
2019-04-16 15:21:22 +0500

    📦 NEW: CSS webpack config for dev and prod modes

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@
  * Webpack Configuration File.
  */
 const path = require( 'path' );
+const autoprefixer = require( 'autoprefixer' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
 
 /**
  * Given a string, returns a new string with dash separators converted to
@@ -42,19 +45,79 @@ const externals = [
 	}
 );
 
+const PluginStylesheetsConfig = ( mode ) => {
+	return [
+		MiniCssExtractPlugin.loader,
+		'css-loader',
+		{
+			loader: 'postcss-loader',
+			options: {
+				ident: 'postcss',
+				plugins: [
+					autoprefixer({
+						browsers: [
+							'>1%',
+							'last 4 versions',
+							'Firefox ESR',
+							'not ie < 9' // React doesn't support IE8 anyway
+						],
+						flexbox: 'no-2009'
+					})
+				]
+			}
+		},
+		{
+			loader: 'sass-loader',
+			options: {
+				outputStyle: 'production' === mode ? 'compressed' : 'nested'
+			}
+		}
+	];
+};
+
+const recursiveIssuer = ( m ) => {
+	if ( m.issuer ) {
+		return recursiveIssuer( m.issuer );
+	} else if ( m.name ) {
+		return m.name;
+	} else {
+		return false;
+	}
+};
+
 module.exports = ( env, options ) => {
 	const mode = options.mode;
 	const suffix = 'production' === mode ? '.min' : '';
 
 	const config = {
-		watch: true,
+		watch: 'development' === mode ? true : false,
 		entry: {
-			'./file-changes': './assets/js/src/file-changes.js',
-			'./settings': './assets/js/src/settings.js'
+			'file-changes': './assets/js/src/file-changes.js',
+			'settings': './assets/js/src/settings.js',
+			'settings': './assets/css/src/settings.scss'
 		},
 		output: {
 			path: path.resolve( __dirname, 'assets/js/dist' ),
 			filename: `[name]${suffix}.js`
+		},
+		optimization: {
+			splitChunks: {
+				cacheGroups: {
+					settingsStyles: {
+						name: 'settings',
+						test: ( m, c, entry = 'settings' ) => 'CssModule' === m.constructor.name && recursiveIssuer( m ) === entry,
+						chunks: 'all',
+						enforce: true
+					}
+
+					// editorStyles: {
+					// 	name: 'editor',
+					// 	test: ( m, c, entry = 'editor' ) => 'CssModule' === m.constructor.name && recursiveIssuer( m ) === entry,
+					// 	chunks: 'all',
+					// 	enforce: true
+					// }
+				}
+			}
 		},
 		devtool: 'development' === mode ? 'cheap-eval-source-map' : false,
 		module: {
@@ -70,10 +133,20 @@ module.exports = ( env, options ) => {
 							cacheDirectory: true
 						}
 					}
+				},
+				{
+					test: /\.s?css$/,
+					use: PluginStylesheetsConfig( mode )
 				}
 			]
 		},
-		externals: externals
+		externals: externals,
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: `../../css/dist/[name]${suffix}.css`
+			}),
+			new FixStyleOnlyEntriesPlugin()
+		]
 	};
 
 	return config;
