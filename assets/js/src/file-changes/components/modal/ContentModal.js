d9d3964f007049f3c289c032efb2e7e46f7ff5bf
Ashar Irfan <mrasharirfan@gmail.com>
2019-04-18 14:57:19 +0500

    📦 NEW: Directory event files modal

---
+++ assets/js/src/file-changes/components/modal/ContentModal.js
@@ -0,0 +1,73 @@
+/**
+ * WordPress Modal.
+ */
+import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+export default class ContentModal extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			modalIsOpen: false
+		};
+
+		this.openModal = this.openModal.bind( this );
+		this.closeModal = this.closeModal.bind( this );
+	}
+
+	openModal() {
+		this.setState({modalIsOpen: true});
+	}
+
+	closeModal() {
+		this.setState({modalIsOpen: false});
+	}
+
+	render() {
+		const { eventFiles } = this.props;
+
+		const filesTable = eventFiles.reduce( ( table, singleFile ) => {
+			table.push( <tr><td>{singleFile.file}</td></tr> );
+			return table;
+		}, []);
+
+		return (
+			<React.Fragment>
+				<button className="btn-event-content" onClick={this.openModal}><span className="dashicons dashicons-info"></span></button>
+				<Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} style={modalStyles} contentLabel="WPFCM Event File Changes">
+					<div className="wpfcm-modal-header">
+						<h2>List of Event Files</h2>
+						<button className="button" onClick={this.closeModal}><span class="dashicons dashicons-no-alt"></span></button>
+					</div>
+					<div className="wpfcm-modal-body">
+						<p>Number of files: {eventFiles.length}</p>
+						<table className="wp-list-table widefat fixed striped">
+							<thead><td>Filename</td></thead>
+							<tbody>{filesTable}</tbody>
+						</table>
+					</div>
+				</Modal>
+			</React.Fragment>
+		);
+	}
+}
+
+const modalStyles = {
+	content: {
+		top: '35%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-40%, -30%)',
+		border: 'none',
+		borderRadius: '0',
+		padding: '0 16px 16px',
+		width: '650px'
+	}
+};
+
+Modal.defaultStyles.overlay.backgroundColor = 'rgba(0,0,0,0.5)';
+Modal.setAppElement( '#wpfcm-file-changes-views' );
