68ebf1c499a66c2733bc24a476f37345cd4ca338
Ashar Irfan <mrasharirfan@gmail.com>
2019-04-18 14:58:09 +0500

    👌 IMPROVE: Events table header

--- assets/js/src/file-changes/components/events-table/EventsTableHead.js
+++ assets/js/src/file-changes/components/events-table/EventsTableHead.js
@@ -9,10 +9,12 @@ export default class EventsTableHead extends Component {
 		return (
 			<thead>
 				<td className="check-column"><input type="checkbox" name="select-all" checked={this.props.selectAll} onChange={this.props.selectAllEvents} /></td>
-				<th>File Path</th>
-				<th>Filename</th>
-				<th className="column-categories">Mark as Read</th>
-				<th className="column-categories">Add to Exclude</th>
+				<th>Path</th>
+				<th className="column-event-name">Name</th>
+				<th className="column-content-type">Type</th>
+				<th className="column-event-action">Mark as Read</th>
+				<th className="column-event-action">Add to Exclude</th>
+				<th className="column-event-content"></th>
 			</thead>
 		);
 	}
